f20ed1a4dbb6134d9934dcc97fea43ebb180ec8c
dariada <dariadadov@gmail.com>
2016-07-14 12:47:47 +0300

    signOut

--- app/public/js/login.js
+++ app/public/js/login.js
@@ -4,4 +4,12 @@
   console.log('Name: ' + profile.getName());
   console.log('Image URL: ' + profile.getImageUrl());
   console.log('Email: ' + profile.getEmail());
+  window.location = "mainmenu.html";
 }
+
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+  }

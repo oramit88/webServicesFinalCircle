ad38d895ff5cac4271ad4544991e0b92b0a1c6c2
dariada <dariadadov@gmail.com>
2016-07-14 12:23:55 +0300

    New app

--- app/public/js/categories.js
+++ app/public/js/categories.js
@@ -1,10 +1,10 @@
-var circleApp = angular.module('circleApp',[]);
+ var circleApp = angular.module('circleApp',[]);
+
 var model = {
         
  };
 
 circleApp.run(function($http){
-//getting a list of categories from mongo collection
   $http.get("https://circlews.herokuapp.com/getAllCategories").success(function(data){
     console.log(data);
     model.categories = data[0].categories;
--- app/public/js/favorites.js
+++ app/public/js/favorites.js
@@ -1,14 +1,15 @@
 var favoritesApp = angular.module('favoritesApp',[]);
+
+
  var model = {
         
   };
 
  favoritesApp.run(function($http){
-    console.log("test2");
-    //sending request to the server in order to bring all current user favorite events from mongoDB.
+     console.log("test2");
     $http.get("https://circlews.herokuapp.com/getEventsByUser").success(function(data){
-        console.log(data);
-        model.favoritesEvents = data;
+     console.log(data);
+      model.favoritesEvents = data;
     });
  });
 
